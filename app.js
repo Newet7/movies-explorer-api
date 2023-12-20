7828e64f305f069fa8fee10f691846def3c3af74
Evgeniy <Newet777@gmail.com>
2023-12-20 11:08:50 +0300

    lvl1: creat app.js + express/mg

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require("express");
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express();
+const {
+  PORT = 3000,
+} = process.env
+const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb'
+
+const app = express()
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log(`Connected to database on ${DATABASE_URL}`)
+  })
+  .catch((err) => {
+    console.log('Error on database connection')
+    console.error(err)
+  })
 
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
+  console.log(`App started on port ${PORT}...`)
+})
